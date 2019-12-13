d11d3fcfbdeea1f78491d7188227d18ea10bc49b
Athagod <afordhal10@mail.bw.edu>
2019-12-12 23:16:50 -0500

    changes

---
+++ README.md
@@ -0,0 +1,3 @@
+Hello, 
+
+This a function calender used to help you keep track of you daily activies. 
---
+++ script.js
@@ -0,0 +1,5 @@
+var saveBtn = document.querySelector("#saveBtn")
+var msgDiv = document.querySelector("#text")
+
+saveBtn.addEventListener("click")
+ console.log(saveBtn)
