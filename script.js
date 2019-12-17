f6209aca56460085d59c5a7b972c47b3c139073d
Athagod <afordhal10@mail.bw.edu>
2019-12-16 23:25:43 -0500

    addition

--- script.js
+++ script.js
@@ -1,13 +1,62 @@
-var saveBtn = document.querySelector("#saveBtn")
-var msgDiv = document.querySelector("#text")
+// how do I create the 9-5, text box and save box //
+// how do get the current hour 
+// how do i determin the current future and past hours 
+// how do I save my input text 
+function loadPage() {
+   var workHours = [9, 10, 11, 12, 1, 2, 3, 4, 5]
 
-saveBtn.addEventListener("click")
- console.log(saveBtn)
+   var currentHour = moment().hour()
+
+   var color = "green"
 
 
- var notes = {
+   for (let index = 0; index < workHours.length; index++) {
+      var inputGroup = $("<div class='input-group'>")
 
-    text: msgDiv.value
- }
+      var dayTime = " am"
+      if ((workHours[index] +12) >= 12) {
+         dayTime = " pm"
+      }
+      var label = $("<label>").text(workHours[index] + dayTime)
 
-localStorage.setItem("notes", notes)
+      console.log((workHours[index] + 12))
+
+      if (workHours[index] >= 1 && workHours[index] <= 5) { //future
+         color = "green"
+      }
+      else if (currentHour > workHours[index]) {  //past
+         color = "grey"
+      }
+
+      else if (currentHour === workHours[index]  ) { //current
+         color = "red"
+      }
+     else{
+      color = "green"
+     }
+
+      var input = $(`<input type='text' style ='background-color:${color}  ' >`)
+
+      var button = $("<button id='saveBtn'>").text("Save")
+
+      inputGroup.append(label, input, button)
+
+      $("#display").append(inputGroup)
+   }
+}
+
+loadPage()
+
+// var saveBtn = document.querySelector("#saveBtn")
+// var msgDiv = document.querySelector("#text")
+
+// saveBtn.addEventListener("click")
+//  console.log(saveBtn)
+
+
+//  var notes = {
+
+//     text: msgDiv.value
+//  }
+
+// localStorage.setItem("notes", notes)
